c5dc9494cb0f22fe4e45a6f4f41a953dbd591c56
Chase Crowder <gcrowder93@gmail.com>
2021-12-06 15:09:31 -0800

    got blogs to pop up

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import React from 'react';
 import './App.css';
 import Header from './components/layout/Header/Header';
 import Footer from './components/layout/Footer/Footer';
+import { useState, useEffect } from 'react';
+import { getBlogs } from './services/blogs';
+import BlogCard from './components/BlogCard/BlogCard';
 
 function App() {
+  // const [title, subtitle, text, image] = useState('');
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const blogData = await getBlogs();
+      setBlogs(blogData);
+      // console.log(blogData);
+    };
+    fetchData();
+  }, []);
   return (
     <section className="main">
       <Header />
       <Footer />
+      {/* <BlogCard {...{ title, subtitle, text, image }} /> */}
+      {blogs.map((blog) => (
+        <BlogCard key={blog.id} {...blog} />
+      ))}
     </section>
   );
 }
-
 export default App;
